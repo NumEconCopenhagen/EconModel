150fdd7ecb090d6cdaf2f95154e4413d4b0f5e23
Jeppe Druedahl <jeppe.druedahl@econ.ku.dk>
2021-06-06 08:56:15 +0200

    Update of download url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='EconModel',
     url='https://github.com/NumEconCopenhagen/EconModel',
-    download_url='https://github.com/NumEconCopenhagen/EconModel/archive/v_01.tar.gz',
+    download_url='https://github.com/NumEconCopenhagen/EconModel/archive/refs/tags/v0.1.tar.gz',
     version='0.1',
     author='Jeppe Druedahl',
     packages=setuptools.find_packages(),
