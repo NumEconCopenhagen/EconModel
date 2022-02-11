698b53f8ab6a599831b540cce504c3fa891b3d45
= <=>
2022-02-11 08:04:23 +0100

    Update to v0.12

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 setuptools.setup(
     name='EconModel',
     url='https://github.com/NumEconCopenhagen/EconModel',
-    download_url='https://github.com/NumEconCopenhagen/EconModel/archive/refs/tags/v0.11.tar.gz',
-    version='0.11',
+    download_url='https://github.com/NumEconCopenhagen/EconModel/archive/refs/tags/v0.12.tar.gz',
+    version='0.12',
     author='Jeppe Druedahl',
     packages=setuptools.find_packages(),
 )
